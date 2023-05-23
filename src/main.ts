995f905c5a8c1c1435accbb405d52c7755af86b4
Víctor Fernández <victor.fernandez.gabriel@gmail.com>
2023-05-23 18:48:06 +0200

    feat: move figures

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { Board } from './board.ts'
 import { Game } from './game.ts'
 import './style.css'
 
-new Game(new Board(20, 10), 'web').start()
+const game = new Game(new Board(20, 10), 'web')
+game.start()
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowLeft') game.move('left')
+  if (event.key === 'ArrowRight') game.move('right')
+  if (event.key === 'ArrowDown') game.move('down')
+  if (event.key === 'ArrowUp') game.rotate(1)
+})
